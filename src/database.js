1ca41ca342c4376587daaf698e0910dc84191422
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-03-03 13:29:41 +0100

    db connection

---
+++ src/database.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import config from "./config";
+
+// "tasksapi" -> nombre de la base de datos
+
+(async () => {
+  const db = await mongoose.connect(config.mongodbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Database connected to: ", db.connection.name);
+})();
+
+// export default db;
