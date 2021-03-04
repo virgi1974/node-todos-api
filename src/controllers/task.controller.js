8155853a2a2cb1c1fb0b3b6632e23ef3f1dd91b8
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-03-04 05:47:44 +0100

    Refactoring controller for success and failure scenarios

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,28 +1,59 @@
 import Task from "../models/Task";
 
 export const findAllTasks = async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || "something went wrong retrieving the tasks",
+    });
+  }
 };
 
 export const createTask = async (req, res) => {
-  // console.log(req.body);
-  const newTask = new Task({
-    title: req.body.title,
-    description: req.body.description,
-    done: req.body.done ? req.body.done : false,
-  });
-  const taskSaved = await newTask.save();
-  console.log(taskSaved);
-  res.json(taskSaved);
-  // console.log("new task created");
+  if (!req.body.title || !req.body.description) {
+    return res.status(400).send({
+      message: "Content can't be empty",
+    });
+  }
+
+  try {
+    const newTask = new Task({
+      title: req.body.title,
+      description: req.body.description,
+      done: req.body.done ? req.body.done : false,
+    });
+    const taskSaved = await newTask.save();
+    console.log(taskSaved);
+    res.json(taskSaved);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || "something went wrong",
+    });
+  }
 };
 
 export const findOneTask = async (req, res) => {
-  const id = req.params.id;
-  const task = await Task.findById(id);
-  res.json(task);
+  const { id } = req.params;
 
+  // #### ERROR HANDLING 1
+  try {
+    const task = await Task.findById(id);
+
+    if (!task)
+      return res
+        .status(404)
+        .send({ message: `task with id ${id} does not exist` });
+
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || `Error retrievingtask with id ${id}`,
+    });
+  }
+
+  // #### ERROR HANDLING 2
   // Task.findById(id)
   //   .exec()
   //   .then(function (task) {
@@ -36,9 +67,16 @@ export const findOneTask = async (req, res) => {
 };
 
 export const deleteTask = async (req, res) => {
-  const id = req.params.id;
-  const task = await Task.findByIdAndDelete(id);
-  res.json({ message: `deleted task with id -> ${id}` });
+  const { id } = req.params;
+
+  try {
+    const task = await Task.findByIdAndDelete(id);
+    res.json({ message: `deleted task with id -> ${id}` });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || `Error deleting task with id ${id}`,
+    });
+  }
 };
 
 export const findAllDoneTasks = async (req, res) => {
