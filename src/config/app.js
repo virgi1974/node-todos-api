abebff95697e9aad6eb4be72f176697e5445f908
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-03-04 05:17:44 +0100

    Accepting html format

--- src/config/app.js
+++ src/config/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+
 import IndexRoutes from "../routes/tasks.routes";
 
 const app = express();
@@ -12,6 +13,7 @@ app.set("port", process.env.PORT || 3000);
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.get("/", (req, res) => {
